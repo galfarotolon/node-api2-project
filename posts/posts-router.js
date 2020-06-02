7393e8405475aa65fd005259cfd5565329519d00
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-02 18:04:30 -0500

    working on mvp

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -17,23 +17,50 @@ router.get('/', (req, res) => {
         });
 });
 
-// router.get('/:id', (req, res) => {
-//     db.findById(req.params.id)
-//         .then(hub => {
-//             if (hub) {
-//                 res.status(200).json(hub);
-//             } else {
-//                 res.status(404).json({ message: 'Hub not found' });
-//             }
-//         })
-//         .catch(error => {
-//             // log error to database
-//             console.log(error);
-//             res.status(500).json({
-//                 message: 'Error retrieving the hub',
-//             });
-//         });
-// });
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+
+
+    Db.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(error => {
+            // log error to database
+            console.log(error);
+            res.status(500).json({
+                error: "The post information could not be retrieved.",
+            });
+        });
+});
+
+router.get('/:id/comments', (req, res) => {
+
+    const postId = req.params.id
+
+
+    Db.findPostComments(postId)
+        .then(comments => {
+            if (comments) {
+                res.status(200).json(comments);
+            }
+            else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(error => {
+            // log error to database
+            console.log(error);
+            res.status(500).json({
+                error: "The posts information could not be retrieved.",
+            });
+        });
+});
 
 router.post('/', (req, res) => {
     const newPost = req.body;
@@ -57,37 +84,26 @@ router.post('/', (req, res) => {
         }
     }
 })
-// .then(post => {
-//     res.status(201).json(post);
-// })
-// .catch(error => {
-//     // log error to database
-//     console.log(error);
-//     res.status(400).json({
-//         errorMessage: "Please provide title and contents for the post.",
-//     });
-// });
-
-//});
 
 
-// router.delete('/:id', (req, res) => {
-//     db.remove(req.params.id)
-//         .then(count => {
-//             if (count > 0) {
-//                 res.status(200).json({ message: 'The hub has been nuked' });
-//             } else {
-//                 res.status(404).json({ message: 'The hub could not be found' });
-//             }
-//         })
-//         .catch(error => {
-//             // log error to database
-//             console.log(error);
-//             res.status(500).json({
-//                 message: 'Error removing the hub',
-//             });
-//         });
-// });
+
+router.delete('/:id', (req, res) => {
+    Db.remove(req.params.id)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: 'The post has been deleted.' });
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(error => {
+            // log error to database
+            console.log(error);
+            res.status(500).json({
+                error: "The post could not be removed",
+            });
+        });
+});
 
 // router.put('/:id', (req, res) => {
 //     const changes = req.body;
